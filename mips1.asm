7b977da91a698480387d55e96a7dca746633a426
Atach0 <167770002+Atach0@users.noreply.github.com>
2026-03-09 13:11:41 -0400

    Update mips1.asm
    Avance macros

--- mips1.asm
+++ mips1.asm
@@ -11,6 +11,81 @@
     syscall
 .end_macro
 
+
+.macro m_ProcesarSigno(%ptr_reg, %signo_reg)
+    lbu $t0, 0(%ptr_reg)    # Cargar el primer byte
+    li %signo_reg, 0        # Por defecto es positivo (0)
+    
+    li $t1, 45              # ASCII de '-'
+    beq $t0, $t1, es_negativo
+    
+    li $t1, 43              # ASCII de '+'
+    beq $t0, $t1, es_positivo
+    j fin_macro             # Si no hay signo, no avanzamos el puntero
+    
+es_negativo:
+    li %signo_reg, 1        # Marcar como negativo
+es_positivo:
+    addi %ptr_reg, %ptr_reg, 1 # Avanzar el puntero para saltar el signo
+    
+fin_macro:
+.end_macro
+
+
+.macro m_AplicarComplemento2(%reg_valor)
+    not %reg_valor, %reg_valor  # Invertir todos los bits (Bitwise NOT)
+    addi %reg_valor, %reg_valor, 1 # Sumar 1
+.end_macro
+
+
+.macro m_ConvertirFraccion(%ptr_reg, %reg_res_frac)
+    # %ptr_reg: Puntero al string despues del '.'
+    # %reg_res_frac: Registro donde guardaremos los 8 bits
+    
+    li %reg_res_frac, 0       # Limpiar el resultado
+    li $t2, 0                 # Contador de bits (m�ximo 8)
+    li $t3, 10                # Constante para multiplicar/dividir
+    
+    # PASO 1: Convertir el string de la fracci�n a un entero
+    # Ej: de ".75" a 75
+    li $t4, 0                 # Valor acumulado
+    li $t5, 1                 # Multiplicador (potencia de 10) para el divisor
+loop_ascii:
+    lbu $t0, 0(%ptr_reg)
+    beq $t0, $zero, iniciar_multiplicacion # Fin de cadena
+    beq $t0, 10, iniciar_multiplicacion    # Salto de l�nea (\n)
+    
+    subi $t0, $t0, 48         # Convertir ASCII a n�mero
+    mul $t4, $t4, 10          # Desplazar decimal
+    add $t4, $t4, $t0         # Sumar d�gito
+    mul $t5, $t5, 10          # Aumentar el divisor (10, 100, 1000...)
+    
+    addi %ptr_reg, %ptr_reg, 1
+    j loop_ascii
+
+iniciar_multiplicacion:
+    # PASO 2: Algoritmo de multiplicaciones sucesivas
+    # t4 = valor fraccionario entero (75)
+    # t5 = el divisor (100)
+loop_bits:
+    beq $t2, 8, fin_fraccion  # Detenerse al llegar a 8 bits 
+    
+    sll %reg_res_frac, %reg_res_frac, 1 # Espacio para el nuevo bit
+    mul $t4, $t4, 2           # Multiplicar por 2
+    
+    blt $t4, $t5, bit_cero    # Si resultado < divisor, el bit es 0
+    
+    # Si resultado >= divisor, el bit es 1
+    ori %reg_res_frac, %reg_res_frac, 1
+    sub $t4, $t4, $t5         # Restar el "entero" (el divisor)
+    
+bit_cero:
+    addi $t2, $t2, 1          # Incrementar contador de bits
+    j loop_bits
+
+fin_fraccion:
+.end_macro
+
 .data
 	Mensaje1: .asciiz "Que formato numerico quieres usar: (decimal empaquetado =a, Complemento a 2 = b, Base 10 =c, Octal =d y Hexadecimal =e)"
 	Mensaje2:  .asciiz "Que formato quiere convertirlo:  (decimal empaquetado =a,  Complemento a 2=b, Base 10 = c, Octal = d y Hexadecimal =e)"
